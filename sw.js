cad235ca02f38282dbc8a4dee42470077c7b6f66
Khai Truong <khai.truong@codelink.io>
2025-03-12 11:37:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -50,9 +50,11 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter(k !== staticCacheName && k !== dynamicCacheName).map((key) => {
-          return caches.delete(key);
-        }),
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => {
+            return caches.delete(key);
+          }),
       );
     }),
   );
