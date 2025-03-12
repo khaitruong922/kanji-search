ca1398e9c659797037d848a1f5618a69326c85a2
Khai Truong <khai.truong@codelink.io>
2025-03-12 11:03:31 +0700

    Revert "Update sw.js"
    This reverts commit 7b0c34ed5b848961492d857118511460cace0d51.

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('fetch', (event) => {
     (async () => {
       try {
         const res = await fetch(event.request);
-        const cache = await caches.open(CACHE_NAME);
+        const cache = await caches.open('cache');
         cache.put(event.request.url, res.clone());
         return res;
       } catch (error) {
