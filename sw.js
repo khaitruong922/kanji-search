702125735a606857c5e468e8658d20b4f6066f86
Khai Truong <khai.truong@codelink.io>
2025-03-12 11:19:24 +0700

    update pwa

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
+const REPO_NAME = '/kanji-search';
 const CACHE_NAME = 'pwa-cache-v1';
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
 const urlsToCache = [
-  'index.html',
-  'index.css',
-  'index.js',
-  'dict.json',
-  'icons/192x192.png',
-  'icons/512x512.png',
+  `${REPO_NAME}/index.html`,
+  `${REPO_NAME}/index.css`,
+  `${REPO_NAME}/index.js`,
+  `${REPO_NAME}/dict.json`,
+  `${REPO_NAME}/icons/192x192.png`,
+  `${REPO_NAME}/icons/512x512.png`,
+  `${REPO_NAME}/offline.html`,
 ];
 
 // Install the service worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(staticCacheName).then((cache) => {
       return cache.addAll(urlsToCache);
     }),
   );
@@ -20,22 +24,34 @@ self.addEventListener('install', (event) => {
 // Fetch resources
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
+    caches
+      .match(event.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(event.request).then((fetchRes) => {
+            return caches.open(dynamicCacheName).then((cache) => {
+              cache.put(event.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (event.request.url.indexOf('.html') > -1) {
+          return caches.match(`${REPO_NAME}/offline.html`);
+        }
+      }),
   );
 });
 
 // Activate and update the cache
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then((keys) => {
       return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
+        keys.filter(k !== staticCacheName && k !== dynamicCacheName).map((key) => {
+          return caches.delete(key);
         }),
       );
     }),
